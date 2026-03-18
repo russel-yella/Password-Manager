393b2357b0ae56a59ec66a1cf897da1234ae1d3b
Yella Russel <yellarussel@gmail.com>
2026-03-18 10:50:57 +0100

    Added search feature, switched from .txt  to .json format, did UI improvements  and improved password generator

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 from tkinter import messagebox
 import pyperclip
 import random
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
@@ -30,24 +31,55 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = { website: { "email": email, "password": password}}
 
     # Check to see if any field is empty
-    if len(website) == 0 or len(email) == 0 or len(password) == 0:
+    if len(website.strip()) == 0 or len(email.strip()) == 0 or len(password.strip()) == 0:
         messagebox.showerror("Error", "Please fill all fields")
 
+    try:
+        with open("data.json", "r") as file:
+            data = json.load(file)
+
+    except (FileNotFoundError, json.JSONDecodeError):
+        data = {}
+
+    if website in data.keys():
+        overwrite = messagebox.askyesno("Warning", f"{website} already exists. Overwrite?")
+        if not overwrite:
+            return
+
+    messagebox.askokcancel(title=website, message=f"These are your details:\n "
+                                                          f"Email: {email}\n "
+                                                          f"Password: {password} \n"
+                                                          "Is it okay to save")
+
+    data.update(new_data)
+
+    with open("data.json", "w") as file:
+        json.dump(data, file, indent=4)
+
+    website_entry.delete(0, END)
+    password_entry.delete(0, END)
+
+def search():
+    website = website_entry.get()
+
+    try:
+        with open("data.json", "r") as file:
+            data = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        messagebox.showerror("Error", "No Data Found")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are your details:\n "
-                                                    f"Email: {email}\n "
-                                                    f"Password: {password} \n"
-                                                    "Is it okay to save")
+        if website in data.keys():
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title="Credentials",
+                                              message=f"Email: {email}"
+                                              f"\nPassword: {password}\n")
+        else:
+            messagebox.showinfo("Credentials", "No credentials found")
 
-
-        if is_ok:
-
-                with open("data.txt", "a") as file:
-                    file.write(f"{website}|| {email} || {password} \n")
-                    website_entry.delete(0, END)
-                    password_entry.delete(0, END)
 
 
 
@@ -92,6 +124,10 @@ password_entry.grid(row=3, column=1, sticky="ew")
 
 generate_password_button = ttk.Button(window, text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2, sticky="ew")
+
+# Search button
+search_button = ttk.Button(window, text="Search", command=search)
+search_button.grid(row=1, column=2, sticky="ew")
 
 # Add Button
 add_password_button = ttk.Button(window, text="Add Password", command=save)
